a23ffbd71cd73fb7c22a50b760a8fa9eba4768fb
JeversonShimMingMing123 <96764987+JeversonShimMingMing123@users.noreply.github.com>
2022-11-10 18:37:02 +0800

    Added button B

--- main.ts
+++ main.ts
@@ -29,6 +29,37 @@ input.onButtonPressed(Button.A, function () {
     zoombit.brake()
     music.playMelody("A B A B A B A B ", 120)
 })
+input.onButtonPressed(Button.B, function () {
+    zoombit.move(MotorDirection.Forward, 128)
+    basic.pause(3800)
+    zoombit.brake()
+    rekabit.setRgbPixelColor(0, 0x000000)
+    basic.pause(100)
+    rekabit.setAllRgbPixelsColor(0x00ffff)
+    rekabit.setAllRgbPixelsColor(0x000000)
+    for (let index = 0; index < 4; index++) {
+        rekabit.setRgbPixelColor(1, 0xff8000)
+        basic.pause(100)
+    }
+    zoombit.turn(TurnDirection.Left, 128)
+    basic.pause(650)
+    zoombit.brake()
+    zoombit.move(MotorDirection.Forward, 132)
+    basic.pause(2000)
+    zoombit.brake()
+    for (let index = 0; index < 4; index++) {
+        rekabit.setRgbPixelColor(1, 0xff8000)
+        basic.pause(100)
+        rekabit.setRgbPixelColor(1, 0x000000)
+        basic.pause(100)
+    }
+    zoombit.turn(TurnDirection.Left, 128)
+    basic.pause(500)
+    zoombit.move(MotorDirection.Forward, 128)
+    basic.pause(2000)
+    zoombit.brake()
+    music.playMelody("A B A B A B A B ", 120)
+})
 basic.showString("Bog bog")
 rekabit.clearAllRgbPixels()
 basic.forever(function () {
@@ -36,7 +67,7 @@ basic.forever(function () {
         . . # . .
         . . # . .
         . . # . .
-        # # . # #
-        # # . # #
+        # # # # #
+        # # # # #
         `)
 })
